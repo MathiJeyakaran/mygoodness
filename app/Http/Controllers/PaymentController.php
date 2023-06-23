2acd0a08343d1ea90d488aaf4ba54ef255db46da
kanchan-salame <hemantsalame54@gmail.com>
2023-06-23 08:34:38 +0530

    Fixed and generate chain link

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,7 +9,6 @@ use App\Models\Transaction;
 use App\Models\User;
 use Session;
 use Illuminate\Support\Str;
-use Illuminate\Support\Str;
 class PaymentController extends Controller
 {
     public function pay(Request $request)
@@ -161,7 +160,7 @@ class PaymentController extends Controller
             $data->transaction_id = $resultPay->transaction->id;
             $data->transaction_amount = $request->totalAmount;
             $data->donation_amount = $request->donationAmount;
-            $data->chain = $request->chain;
+            $data->chain = Str::random(30);
             $data->charity_ein = $request->charityEin;
             $data->nonprofit = $request->charityName;
             $data->save();
