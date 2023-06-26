bf6f1ae0fcc4245f40f39f87d8f718e46f35c8bf
Kanchan <kanchan@atinatechnology.in>
2023-06-26 07:17:39 +0530

    added https redirection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Providers\Braintree_Configuration;
 use Braintree\Configuration;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Contracts\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +17,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //check that app is local
+        if ($this->app->isLocal()) {
+            //if local register your services you require for development
+            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+        } else {
+            //else register your services you require for production
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -25,6 +34,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'development') {
+            URL::forceScheme('https');
+        }
         Configuration::environment(env('BRAINTREE_ENV'));
         Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
         Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
